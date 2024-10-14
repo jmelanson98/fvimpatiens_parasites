68f57e3842679daa00fbdd3ecebec4d656ee0759
jmelanson98 <168047375+jmelanson98@users.noreply.github.com>
2024-10-13 20:41:28 -0700

    updating models to include caste as predictor

--- src/runParasiteModels.R
+++ src/runParasiteModels.R
@@ -4,10 +4,25 @@ runParasiteModels <- function(spec.data,
                               xvars){
 
   formula.parasite  <- as.formula(paste(
-    paste(parasite, "| weights(weightsPar)"),  #+ trials(1)
+    paste(parasite, "| subset(subsetPar)"),  #+ trials(1)
     paste(xvars,
           collapse=" + "),
     sep=" ~ "))
 
+  return(formula.parasite)
+}
+
+
+
+runParasiteModelsNoSubset <- function(spec.data,
+                              parasite,
+                              xvars){
+  
+  formula.parasite  <- as.formula(paste(
+    paste(parasite),  #+ trials(1)
+    paste(xvars,
+          collapse=" + "),
+    sep=" ~ "))
+  
   return(formula.parasite)
 }
